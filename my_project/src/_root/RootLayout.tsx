b884746738005b28d205121531eeaf32edfb77de
Guilherme <guilhermeco@gmail.com>
2025-10-14 23:33:35 -0300

    Added new packages: @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, tailwind-merge, and tw-animate-css. Updated @types/node version and modified tsconfig for path aliases. Refactored RootLayout component to use a Button from the UI library.

--- my_project/src/_root/RootLayout.tsx
+++ my_project/src/_root/RootLayout.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
 const RootLayout = () => {
   return (
-    <div>
-      <h1 className="text-white">RootLayout</h1>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
     </div>
   );
 };
