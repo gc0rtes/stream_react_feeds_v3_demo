696a3c6186324262808d4acf8ffd5f37ff106274
Guilherme <guilhermeco@gmail.com>
2025-10-28 17:34:26 -0300

    Add new routes and pages to App component for enhanced navigation

--- my_project/src/App.tsx
+++ my_project/src/App.tsx
@@ -3,7 +3,17 @@ import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
-import { Home } from "./_root/pages";
+import {
+  Home,
+  Explore,
+  AllUsers,
+  Saved,
+  CreatePost,
+  UpdatePost,
+  PostDetails,
+  Profile,
+  UpdateProfile,
+} from "./_root/pages";
 
 import { Toaster } from "./components/ui/sonner";
 
@@ -22,9 +32,16 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<UpdatePost />} />
+          <Route path="/post/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
-
       <Toaster />
     </main>
   );
