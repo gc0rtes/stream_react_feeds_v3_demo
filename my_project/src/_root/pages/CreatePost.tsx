8d407d334898ad2d00c9f725ba407ef008d4409d
Guilherme <guilhermeco@gmail.com>
2025-10-30 07:36:08 -0300

    Start yo Add CreatePost component.

--- my_project/src/_root/pages/CreatePost.tsx
+++ my_project/src/_root/pages/CreatePost.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="flex-start gap-3 w-full max-w-5xl">
+          <img
+            src="/assets/icons/add-post.svg"
+            width={36}
+            height={36}
+            alt="add"
+          />
+          <h2 className="h3-bold md:h2-bold text-left w-full">Create Post</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
