a0ebe58dd3c8a7a13fbb59f48e20c35d069dba1c
Guilherme <guilhermeco@gmail.com>
2025-11-23 15:29:55 -0300

    Refactor Saved component to enhance layout and styling. Add header with bookmark icon

--- my_project/src/_root/pages/Saved.tsx
+++ my_project/src/_root/pages/Saved.tsx
@@ -19,7 +19,22 @@ const Saved = () => {
   if (!bookmarksData || activities.length === 0)
     return <p className="text-light-4">No saved posts</p>;
 
-  return <GridPostList posts={activities} showStats={false} />;
+  return (
+    <div className="flex flex-col flex-1 items-center overflow-scroll py-10 px-5 md:p-14 custom-scrollbar">
+      <div className="max-w-5xl flex flex-col items-center w-full gap-6 md:gap-9">
+        <div className="flex items-center justify-start w-full gap-2">
+          <img
+            src="/assets/icons/bookmark.svg"
+            alt="bookmark"
+            width={24}
+            height={24}
+          />
+          <h2 className="h3-bold md:h2-bold text-left w-full">Saved Posts</h2>
+        </div>
+        <GridPostList posts={activities} showStats={false} />
+      </div>
+    </div>
+  );
 };
 
 export default Saved;
--- my_project/src/components/shared/LeftSidebar.tsx
+++ my_project/src/components/shared/LeftSidebar.tsx
@@ -97,20 +97,21 @@ const LeftSidebar = () => {
           })}
         </ul>
       </div>
-
-      <Button
-        variant="ghost"
-        className="shad-button_ghost"
-        onClick={handleSignOut}
-      >
-        <img
-          src="/assets/icons/logout.svg"
-          alt="logout"
-          width={20}
-          height={20}
-        />
-        <p className="small-medium lg:base-medium ">Logout</p>
-      </Button>
+      <div>
+        <Button
+          variant="ghost"
+          className="shad-button_ghost"
+          onClick={handleSignOut}
+        >
+          <img
+            src="/assets/icons/logout.svg"
+            alt="logout"
+            width={20}
+            height={20}
+          />
+          <p className="small-medium lg:base-medium ">Logout</p>
+        </Button>
+      </div>
     </nav>
   );
 };
