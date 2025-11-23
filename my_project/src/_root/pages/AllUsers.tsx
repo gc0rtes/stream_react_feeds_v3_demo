eddd0664b2909cacea784fcc2683c8e74f55759e
Guilherme <guilhermeco@gmail.com>
2025-11-23 20:11:10 -0300

    Add detailed descriptions to AllUsers component for user guidance and functionality overview

--- my_project/src/_root/pages/AllUsers.tsx
+++ my_project/src/_root/pages/AllUsers.tsx
@@ -1,5 +1,25 @@
 const AllUsers = () => {
-  return <div>AllUsers</div>;
+  return (
+    <div>
+      AllUsers
+      <p>
+        {" "}
+        This will use the follows API to get the list of users tha you already
+        follow
+      </p>
+      <p> We will display the users in a grid </p>
+      <p>
+        {" "}
+        The suggestion follow API will be used to suggest user so user can
+        follow{" "}
+      </p>
+      <p>
+        {" "}
+        this screen will be more a friends page that you already follow/is
+        friend
+      </p>
+    </div>
+  );
 };
 
 export default AllUsers;
